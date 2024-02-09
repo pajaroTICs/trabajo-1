f122d967ad8f759558389e873a981aa9a1d61a23
= <=>
2024-02-09 12:07:49 -0600

    agregue un formulario

--- JavaScript/fuctions.js
+++ JavaScript/fuctions.js
@@ -1,8 +1,8 @@
 function changeColor(){
  document.getElementById("palabrita").style.backgroundColor="green";
-document.getElementById("palabrita").style.color="white";
-
-
-
+document.getElementById("palabrita").textContent="hola";
 }
-
+ 
+function enviarMsg(){
+   alert("hola amigo");
+}
